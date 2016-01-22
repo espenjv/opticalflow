27ad01db12ead35c47ec7f3ae3f62fa8613cd78e
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2016-01-22 15:54:15 +0100

    Using conv2 matlab function to convolve the filter

--- code/sobelFilter.m
+++ code/sobelFilter.m
@@ -1,12 +1,10 @@
-function [Gx,Gy] = sobelFilter(im)
+function [Dx,Dy] = sobelFilter(g)
 %sobelFilter Apply sobelfilter to an image
 %   Detailed explanation goes here
-
-    [m,n] = size(im);
 
     Sx = [-1 0 1; -2 0 2; -1 0 1];
     Sy = Sx';
     
-    Gx = conv2(double(im),double(Sx));
-    Gy = conv2(double(im),double(Sy));
+    Dx = conv2(double(g),double(Sx));
+    Dy = conv2(double(g),double(Sy));
 end
