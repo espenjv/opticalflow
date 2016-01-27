47293f83b5c4e904fc37658b2c0ea4aee6f8f429
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2016-01-27 21:10:38 +0100

    changed from uint8 to double type

--- code/timeDisc.m
+++ code/timeDisc.m
@@ -2,7 +2,7 @@ function c = timeDisc( g1, g2)
 %timeDisc Computes the time discretization of the time derivative
 %   Detailed explanation goes here
 
-    c = g2-g1;
+    c = double(g2)-double(g1);
 
 end
 
