fa93a1190c91f30cf323bf1ddacbcf5b4633fffa
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2016-01-27 21:08:09 +0100

    Reformated the gradient matrix

--- code/sobelFilter.m
+++ code/sobelFilter.m
@@ -4,7 +4,10 @@ function [Dx,Dy] = sobelFilter(g)
 
     Sx = [-1 0 1; -2 0 2; -1 0 1];
     Sy = Sx';
+    [m,n] = size(g);
     
     Dx = conv2(double(g),double(Sx));
+    Dx = Dx(2:m+1,2:n+1);
     Dy = conv2(double(g),double(Sy));
+    Dy = Dy(2:m+1,2:n+1);
 end
