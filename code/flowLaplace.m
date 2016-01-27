36e4df92eeec9f3460099e6f5ce0604d5408d735
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2016-01-27 21:07:32 +0100

    Corrected dimensions. Not USED in solveSystem.m

--- code/flowLaplace.m
+++ code/flowLaplace.m
@@ -1,9 +1,10 @@
-function [Lx,Ly] = flowLaplace(m,n)
+function L = flowLaplace(m,n)
 %flowLaplace Computes the Laplace matrices
 %   Detailed explanation goes here
     
-    Ly = diag(ones(m-1,1),-1) -2*diag(ones(m,1)) + diag(ones(m-1,1),1);
-    Lx = diag(ones(n-1,1),-1) -2*diag(ones(n,1)) + diag(ones(n-1,1),1);
+    L = diag(ones(m+n-1,1),-1) -2*diag(ones(m+n,1)) + diag(ones(m+n-1,1),1);
+    L(m+1,m) = 0;
+    L(m,m+1) = 0;
 
 end
 
