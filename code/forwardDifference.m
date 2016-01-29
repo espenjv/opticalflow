ac5044947032a83ceedb045b4f5a4a310abac11c
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2016-01-29 15:43:35 +0100

    Corrected forwardDifference

--- code/forwardDifference.m
+++ code/forwardDifference.m
@@ -1,9 +1,14 @@
 function L = forwardDifference(m,n)
 %forwardDifference Forms the forwardDifference matrix
 %   Detailed explanation goes here
-
-    L = sparse(1:2*m*n,1:2*m*n,-ones(2*m*n,1),2*m*n,2*m*n) + sparse(1:2*m*n-1,2:2*m*n,ones(2*m*n-1,1),2*m*n,2*m*n);
-    L(m*n,m*n+1) = 0;
+    
+    Lx = sparse(1:m*n,1:m*n,-ones(m*n,1),m*n,m*n) + sparse(1:m*(n-1),m+1:m*n,ones((n-1)*m,1),m*n,m*n);
+    Ly = kron(eye(n),sparse(1:m,1:m,-ones(m,1),m,m) + sparse(1:m-1,2:m,ones(m-1,1),m,m));
+    
+    L1 = sparse(2*m*n,m*n);
+    L1(1:m*n,1:m*n) = Lx;
+    L1(m*n+1:2*m*n,1:m*n) = Ly;
+    L = kron(eye(2),L1);
 
 end
 
