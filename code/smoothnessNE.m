0e79e1666086c39870144a61d7c4394710554c9e
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2016-02-12 12:27:48 +0100

    Function for computing the smoothness term of Nagel and Enkelmann

--- code/smoothnessNE.m
+++ code/smoothnessNE.m
@@ -1,21 +1,22 @@
-function V = smoothnessNE(Dx,Dy,m,n)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-    
-    grad = sqrt(Dx.^2+Dy.^2);
-    sx = Dx./grad;
-    sy = Dy./grad;
+function V = smoothnessNE(Dx,Dy,m,n,kappa)
+%smoothnessNE Computes the smoothness
+%   Computes the smoothness term using the method of Nagel and Enkelmann.
+%   The method uses a projection matrix P to give different wheights to the
+%   components of the flow deriatives in the direction of the image
+%   gradient and the orthogonal direction. Kappa is a regularization
+%   parameter > 0.
+
+    grad = Dx.^2+Dy.^2;
+    denom = grad + kappa^2;
+    sx = Dx.^2./denom;
+    sy = Dy.^2./denom;
+    sxy = Dx.*Dy./denom;
   
-    
-    P1 = sparse(1:m*n,1:m*n,sx,2*m*n,2*m*n);
-    P1(1:m*n,m*n+1:2*m*n) = sparse(1:m*n,1:m*n,sy,m*n,m*n);
-    P1(m*n+1:2*m*n,1:m*n) = sparse(1:m*n,1:m*n,-sy,m*n,m*n);
-    P1(m*n+1:2*m*n,m*n+1:2*m*n) = sparse(1:m*n,1:m*n,sx,m*n,m*n);
+    P1 = sparse(1:m*n,1:m*n,sy+kappa^2,2*m*n,2*m*n);
+    P1(1:m*n,m*n+1:2*m*n) = sparse(1:m*n,1:m*n,-sxy,m*n,m*n);
+    P1(m*n+1:2*m*n,1:m*n) = sparse(1:m*n,1:m*n,-sxy,m*n,m*n);
+    P1(m*n+1:2*m*n,m*n+1:2*m*n) = sparse(1:m*n,1:m*n,sx+kappa^2,m*n,m*n);
     P = kron(eye(2),P1);
-    
-    G1 = sparse(1:m*n,1:m*n,grad,m*n,2*m*n);
-    G1(:,m*n+1:2*m*n) = sparse(1:m*n,1:m*n,ones(m*n,1),m*n,m*n);
-    G = [G1 G1];
     
     [Lx,Ly] = forwardDifference(m,n);
     
@@ -24,13 +25,7 @@ function V = smoothnessNE(Dx,Dy,m,n)
     L1(m*n+1:2*m*n,1:m*n) = Ly;
     L = kron(eye(2),L1);
     
-    
-    V = (P*L)'*(P*L);
-    
-    V = L'*L;
-    
-    V = (V>10^-15).*V;
-    
+    V = L'*P*L; 
 
 end
 
